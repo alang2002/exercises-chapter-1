13ffd9c1943b560b2369a269dba92245aa1c5c52
Aaron Lang <blackthornlm@gmail.com>
2017-08-29 20:25:58 -0500

    Update TableAndChairs.java

--- Chapter1/src/exercises/TableAndChairs.java
+++ Chapter1/src/exercises/TableAndChairs.java
@@ -4,29 +4,29 @@ public class TableAndChairs {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("C																     					C");
-		System.out.println("C																     					C");
-		System.out.println("C																     					C");
-		System.out.println("C																	 					C");
-		System.out.println("C																	 					C");
-		System.out.println("C																	 					C");
-		System.out.println("C																	 					C");
-		System.out.println("C																	 					C");
-		System.out.println("C																	 					C");
-		System.out.println("C																	 					C");
-		System.out.println("C																	 					C");
-		System.out.println("C																	 					C");
+		System.out.println("C											    C");
+		System.out.println("C											    C");
+		System.out.println("C								   			    C");
+		System.out.println("C											    C");
+		System.out.println("C											    C");
+		System.out.println("C											    C");
+		System.out.println("C											    C");
+		System.out.println("C										   	    C");
+		System.out.println("C											    C");
+		System.out.println("C											    C");
+		System.out.println("C											    C");
+		System.out.println("C											    C");
 		System.out.println("CCCCCCCCCCCCCCCCCCCCC    ___________________________________         CCCCCCCCCCCCCCCCCCCC");
-		System.out.println("C                   C    T                                 T         C					C");
-		System.out.println("C                   C    T                                 T         C					C");
-		System.out.println("C                   C    T                                 T         C					C");
-		System.out.println("C                   C    T                                 T         C					C");
-		System.out.println("C                   C    T                                 T         C					C");
-		System.out.println("C                   C    T                                 T         C					C");
-		System.out.println("C                   C    T                                 T         C					C");
-		System.out.println("C                   C    T                                 T         C					C");
-		System.out.println("C                   C    T                                 T         C					C");
-		System.out.println("C                   C    T                                 T         C                	C");
+		System.out.println("C                   C    T                                 T         C	     	    C");
+		System.out.println("C                   C    T                                 T         C		    C");
+		System.out.println("C                   C    T                                 T         C		    C");
+		System.out.println("C                   C    T                                 T         C		    C");
+		System.out.println("C                   C    T                                 T         C	       	    C");
+		System.out.println("C                   C    T                                 T         C		    C");
+		System.out.println("C                   C    T                                 T         C		    C");
+		System.out.println("C                   C    T                                 T         C		    C");
+		System.out.println("C                   C    T                                 T         C		    C");
+		System.out.println("C                   C    T                                 T         C                  C");
 		
 	
 	}
